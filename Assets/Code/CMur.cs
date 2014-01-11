263de8c1d5bd060d1c08dbf04269bd28c6441585
LeuhFahn <leuh.fahn@gmail.com>
2014-01-11 22:06:57 +0100

    prefab table

---
+++ Assets/Code/CMur.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CMur : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		float fWidth = gameObject.renderer.material.GetTexture("_MainTex").width;
+		float fHeight = gameObject.renderer.material.GetTexture("_MainTex").height;
+		float fX = gameObject.transform.localScale.x;
+		float fY = gameObject.transform.localScale.y;
+		
+		gameObject.renderer.material.SetTextureScale("_MainTex", new Vector2(fX * 30 / fWidth, fY* 30 / fHeight));	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
