4fbfe3b157b4c2931efd5591db0d14b95b1195b8
LeuhFahn <leuh.fahn@gmail.com>
2014-01-11 20:34:13 +0100

    intro script

--- Assets/Code/CTriggerSortieBureau.cs
+++ Assets/Code/CTriggerSortieBureau.cs
@@ -3,20 +3,51 @@ using System.Collections;
 
 public class CTriggerSortieBureau : MonoBehaviour {
 
+	bool m_bIsActivated;
+	CGame m_Game;
+	float m_fTimerAffichage;
+	const float m_fTimerAffichageMax = 5.0f;
+	const float m_fHeightText = 100;
+
 	// Use this for initialization
 	void Start () 
 	{
-	
+		m_bIsActivated = false;
+		m_Game = GameObject.Find("_Game").GetComponent<CGame>();
+		m_fTimerAffichage = 0.0f;
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-	
+		if(m_fTimerAffichage > 0.0f)
+			m_fTimerAffichage -= Time.deltaTime;
 	}
 
 	void OnTriggerEnter(Collider other)
 	{
+		if(other.CompareTag("Player"))
+		{
+			if(!m_bIsActivated)
+				OutOfBureau();
+		}
+	}
 
+	void OutOfBureau()
+	{
+		m_bIsActivated = true;
+		m_fTimerAffichage = m_fTimerAffichageMax;
+	}
+
+	void OnGUI()
+	{
+
+		GUIStyle centeredStyle = GUI.skin.GetStyle("Label");
+		centeredStyle.alignment = TextAnchor.UpperCenter;
+		if(m_fTimerAffichage > 0.0f)
+		{
+			GUI.skin.label.font = m_Game.m_Font; 
+			GUI.Label(new Rect( 0, m_Game.m_fHeight - m_fHeightText, m_Game.m_fWidth, m_Game.m_fHeight), "Ho Shit, I gotta go to the Particle Accelerator!", centeredStyle);
+		}
 	}
 }
