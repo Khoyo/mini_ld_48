605a70041a1da824274507257382671e5f50ad28
Khoyo <khoyobegenn@gmail.com>
2014-01-12 00:59:00 +0100

    Add the core gameplay

--- Assets/Code/CFissure.cs
+++ Assets/Code/CFissure.cs
@@ -1,15 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class CFissure : MonoBehaviour {
 
+	int m_nombreDeMorceaux = 0;
+	List<bool> soude;
+
+	void Awake() {
+		soude = new List<bool>();
+	}
+
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+		if(!soude.Contains(false))
+			SendMessageUpwards("Activate");
 	}
+
+	void AttributeMorceauNumber(CMorceauFissure morceau){
+		morceau.setID(soude.Count);
+		print(soude.Count);
+		soude.Add(false);
+
+	}
+
+	void SoudageMorceau(int id){
+		soude[id] = true;
+	}
+
 }
