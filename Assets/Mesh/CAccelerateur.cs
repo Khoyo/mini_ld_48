5d3d316fbadd48abbb8aa66558c44d550b28684a
LeuhFahn <leuh.fahn@gmail.com>
2014-01-12 21:33:54 +0100

    sound of door

--- Assets/Mesh/CAccelerateur.cs
+++ Assets/Mesh/CAccelerateur.cs
@@ -4,10 +4,12 @@ using System.Collections;
 public class CAccelerateur : MonoBehaviour {
 
 	bool m_isOpened = false;
+	CGame m_Game;
 
 	// Use this for initialization
-	void Start () {
-
+	void Start ()
+	{
+		m_Game = GameObject.Find("_Game").GetComponent<CGame>();
 	}
 	
 	// Update is called once per frame
@@ -16,8 +18,9 @@ public class CAccelerateur : MonoBehaviour {
 	}
 
 	public void Open(){
-			animation.PlayQueued("Ouverture");
-			m_isOpened = true;
+		animation.PlayQueued("Ouverture");
+		m_isOpened = true;
+		m_Game.GetSoundEngine().postEvent("Play_DoorOver", gameObject);
 	}
 
 	public void Close()
@@ -25,6 +28,7 @@ public class CAccelerateur : MonoBehaviour {
 		if(m_isOpened){
 			animation.PlayQueued("Fermeture");
 			m_isOpened = false;
+			m_Game.GetSoundEngine().postEvent("Play_DoorClose", gameObject);
 		}
 
 	}
