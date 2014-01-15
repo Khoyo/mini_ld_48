419c8b855cab24c643bf12f976b2ba5abfbfc25d
Khoyo <khoyobegenn@gmail.com>
2014-01-15 17:17:12 +0100

    Multi Fissure

--- Assets/Code/CBlocAReparer.cs
+++ Assets/Code/CBlocAReparer.cs
@@ -5,12 +5,13 @@ public class CBlocAReparer : MonoBehaviour
 {
 
 	bool m_isRepaired = false;
+	int repairCount;
 	public GameObject m_gameObjectToActivate;
 	
 	// Use this for initialization
 	void Start () 
 	{
-		
+		repairCount = GetComponentsInChildren<CFissure>().Length;
 	}
 	
 	// Update is called once per frame
@@ -21,8 +22,9 @@ public class CBlocAReparer : MonoBehaviour
 	
 	public void Repair() 
 	{
-		m_isRepaired = true;
-		print ("Repaired");
-		m_gameObjectToActivate.SendMessage("Activate");
+		if(repairCount != 0)
+			m_isRepaired = true;
+			print ("Repaired");
+			m_gameObjectToActivate.SendMessage("Activate");
 	}
 }
