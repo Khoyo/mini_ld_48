4c7b28fe2df0e8b133031b950dedfbdc26d683a1
LeuhFahn <leuh.fahn@gmail.com>
2014-01-16 17:36:36 +0100

    modif

--- Assets/Code/CRecharge.cs
+++ Assets/Code/CRecharge.cs
@@ -3,17 +3,18 @@ using System.Collections;
 
 public class CRecharge : MonoBehaviour 
 {
-
+	Vector3 posInit;
 	// Use this for initialization
 	void Start () 
 	{
-	
+		posInit = gameObject.transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-	
+		Vector3 newPos = posInit + new Vector3(0, Mathf.Cos(Time.deltaTime), 0);
+		gameObject.transform.position = newPos;
 	}
 
 	void OnTriggerEnter(Collider other)
